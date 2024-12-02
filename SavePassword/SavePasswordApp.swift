3ce69d3a483d143cf6b0b5b477bf1caa0a2a4b91
YaelZamora <ajedrezkasparov@gmail.com>
2024-12-02 13:32:00 -0600

    Initial Commit

---
+++ SavePassword/SavePasswordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavePasswordApp.swift
+//  SavePassword
+//
+//  Created by Yael Javier Zamora Moreno on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SavePasswordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
